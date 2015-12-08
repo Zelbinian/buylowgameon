bd57ce36b509a0e8a0ca9d4529a4e6304b40ebe5
Zelbinian <dustin.l.hodge@gmail.com>
2015-12-07 21:20:03 -0800

    Initial file

---
+++ blgo.r
@@ -0,0 +1,7 @@
+# This R script is designed to help me build my boardgame collection.
+# It pulls everything I both want to play and don't own from boardgame geek.
+# Then it uses a pricing enging to figure out which games are the cheapest so I can
+# buy new games I want to play without spending a lot of money!
+
+# first, make sure packages are installed
+require("XML")
